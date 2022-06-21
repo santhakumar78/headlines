0cc771059c52412bdf6fe6a322c940e77540ee19
santhakumar78 <53222095+santhakumar78@users.noreply.github.com>
2022-06-21 18:10:25 +0530

    Wth Jinja Template

--- headlines.py
+++ headlines.py
@@ -1,5 +1,5 @@
 import feedparser
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -12,19 +12,22 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 @app.route("/<publication>")
 def get_news(publication = "bbc"):
     feed = feedparser.parse(RSS_FEEDS[publication])
-    firstArticle = feed['entries'][0]
-    title = firstArticle.get("title")
-    published = firstArticle.get("published")
-    summary = firstArticle.get("summary")
-    return """<html>
-    <body>
-    <h1> Headlines</h1>
-    <b>{0}</b> <br/>
-    <i>{1}</i> <br/>
-    <p>{2}</p><br/>
-    </body>
-    </html>    
-    """.format(firstArticle.get("title"), firstArticle.get("published"), firstArticle.get("summary"))
+
+    return render_template("home.html", 
+        articles = feed['entries'])
+    
+#    title = firstArticle.get("title")
+#   published = firstArticle.get("published")
+#    summary = firstArticle.get("summary")
+#    return """<html>
+#    <body>
+#    <h1> Headlines</h1>
+#    <b>{0}</b> <br/>
+#    <i>{1}</i> <br/>
+#    <p>{2}</p><br/>
+#    </body>
+#    </html>    
+#    """.format(firstArticle.get("title"), firstArticle.get("published"), firstArticle.get("summary"))
 
     
 if __name__ == '__main__':
